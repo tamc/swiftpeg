725e9ebdb9df1e7e01fdc700efbd5c9621d981cf
Thomas Counsell <tom@eyfs.info>
2018-10-04 10:26:34 +0100

    Refactored the calculator example a bit

--- examples/simple-calculator/main.swift
+++ examples/simple-calculator/main.swift
@@ -1,40 +1,60 @@
 import Foundation
 
-func valueFor(_ symbol: Symbol) -> Double {
-    switch symbol {
-    case let c as Calculation:
-        return c.value
-    case let d as Double:
-        return d
-    case let s as String:
-        guard let n = Double(s) else { fatalError("Not a float") }
-        return n
-    default:
-        fatalError("Unrecognised type")
-    }
+enum CalculationError: Error {
+    case notAnOperator(Symbol)
+    case doesNotHaveAValue(Symbol)
 }
 
-class Calculation: Symbol {
-    
-    let value: Double
-    
-    init?(_ symbols: [Symbol]) {
-        var symbols = symbols
-        guard symbols.isEmpty == false else { self.value = 0; return }
-        var v = valueFor(symbols.removeFirst())
-        while symbols.isEmpty == false {
-            guard let o = symbols.removeFirst() as? Operator else { return nil }
-            let otherValue = valueFor(symbols.removeFirst())
-            v = o.apply(v, otherValue)
-        }
-        self.value = v
-    }
+protocol HasValue {
+    var value: Double { get }
+}
+
+extension Double: Symbol, HasValue {
+    var value: Double { return self }
 }
 
 protocol Operator: Symbol {
     init(substring: Substring)
     func apply(_ left: Double, _ right: Double) -> Double
 }
+
+extension Sequence {
+    /**
+    reduceInPairs is like reduce, but will pass the next two elements to the reducing function rather than one. If there are an odd number of elements, the last element will be ignored.
+    */
+    func reduceInPairs<Result>(_ initialResult: Result, _ nextPartialResult: (Result, Element, Element) throws -> Result  ) rethrows -> Result {
+        var result = initialResult
+        var iterator = makeIterator()
+        while let a = iterator.next(), let b = iterator.next() {
+            result = try nextPartialResult(result, a, b)
+        }
+        return result
+    }
+}
+
+class Calculation: Symbol, HasValue {
+    
+    let value: Double
+    
+    init?(_ symbols: [Symbol]) {
+        let leftToRightReduction: (Double, Symbol, Symbol) throws -> Double = { (cummulative, op, right) in
+            guard let o = op as? Operator else { throw CalculationError.notAnOperator(op) }
+            guard let r = right as? HasValue else { throw CalculationError.doesNotHaveAValue(right) }
+            return o.apply(cummulative, r.value)
+        }
+        do {
+            if let start = symbols.first as? HasValue {
+                value = try symbols.dropFirst().reduceInPairs(start.value, leftToRightReduction)
+            } else {
+                value = try symbols.reduceInPairs(0.0, leftToRightReduction)
+            }
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+}
+
 
 struct Add: Operator {
     let substring: Substring
@@ -68,11 +88,9 @@ struct Divide: Operator {
     }
 }
 
-extension Double: Symbol { }
-
 class Calculator: PegParser {
     
-    static let integerRegexp = try! NSRegularExpression(pattern: "[+-]?[0-9]+(.[0-9]+)?([eE][+-]?[0-9]+)?", options: [])
+    static let integerRegexp = try! NSRegularExpression(pattern: "[+-]?[0-9]+(\\.[0-9]+)?([eE][+-]?[0-9]+)?", options: [])
     
     var parseState: ParseState
     
@@ -93,23 +111,32 @@ class Calculator: PegParser {
     }
     
     func sumOperators() -> Symbol? {
-        return terminal("+", Add.init) || terminal("-", Subtract.init)
+        return space() && (terminal("+", Add.init) || terminal("-", Subtract.init))
     }
     
     func productOperators() -> Symbol? {
-        return terminal("*", Multiply.init) || terminal("/", Divide.init)
+        return space() && (terminal("*", Multiply.init) || terminal("/", Divide.init))
     }
     
     func value() -> Symbol? {
-        return (terminal("(") && expression() && terminal(")")) || number()
+        return space() && (brackets() || number())
+    }
+    
+    func brackets() -> Symbol? {
+        return terminal("(") && space() && expression() && space() && terminal(")")
     }
     
     func number() -> Symbol? {
         return terminal(Calculator.integerRegexp, Double.init)
     }
+    
+    func space() -> Symbol? {
+        return zeroOrMore(terminal(" "))
+    }
 }
 
 print("Enter a sum, like 1.0+1.0 and press enter, ctrl-c to exit")
+
 while true {
     let s = readLine(strippingNewline: true)!
     if let c = Calculator(s).expression() as? Calculation {
