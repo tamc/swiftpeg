91f2bd15bc2682be06483f26694da117bb4ac027
Thomas Counsell <tom@eyfs.info>
2018-10-04 07:08:26 +0100

    Linting and tidying

--- examples/textpeg/textpeg-classes.swift
+++ examples/textpeg/textpeg-classes.swift
@@ -3,39 +3,39 @@ import Foundation
 extension String: Symbol {}
 
 protocol IndentedStringConvertible {
-  func indentedDescription(indent: String, depth: Int) -> String
+    func indentedDescription(indent: String, depth: Int) -> String
 }
 
 class NonTerminalClass: Symbol, CustomStringConvertible, IndentedStringConvertible {
-  let children: [Symbol]
-  init?(children: [Symbol]) {
-    self.children = children
-  }
-  var description: String {
-    return indentedDescription()
-  }
-
-  func indentedDescription(indent: String = "  ", depth: Int = 0) -> String {
-    var description: [String] = ["\(String(repeating:indent, count:depth))\(type(of: self))"]
-    for child in children {
-      if let c = child as? IndentedStringConvertible {
-        description.append(c.indentedDescription(indent: indent, depth: depth + 1))
-      } else {
-        description.append("\(String(repeating:indent, count: depth+1))\(String(describing:child))")
-      }
+    let children: [Symbol]
+    init?(children: [Symbol]) {
+        self.children = children
     }
-    return description.joined(separator: "\n")
-  }
+    var description: String {
+        return indentedDescription()
+    }
+    
+    func indentedDescription(indent: String = "  ", depth: Int = 0) -> String {
+        var description: [String] = ["\(String(repeating:indent, count:depth))\(type(of: self))"]
+        for child in children {
+            if let c = child as? IndentedStringConvertible {
+                description.append(c.indentedDescription(indent: indent, depth: depth + 1))
+            } else {
+                description.append("\(String(repeating:indent, count: depth+1))\(String(describing:child))")
+            }
+        }
+        return description.joined(separator: "\n")
+    }
 }
 
 class SymbolClass: Symbol, CustomStringConvertible {
-  let substring: Substring
-  init?(substring: Substring) {
-    self.substring = substring
-  }
-  var description: String {
-    return "\(type(of: self)) \"\(substring)\""
-  }
+    let substring: Substring
+    init?(substring: Substring) {
+        self.substring = substring
+    }
+    var description: String {
+        return "\(type(of: self)) \"\(substring)\""
+    }
 }
 
 class Identifier: SymbolClass {
@@ -45,15 +45,15 @@ class TextPeg: NonTerminalClass {
 }
 
 class Node: NonTerminalClass {
-  override var description: String  {
-    return "\(super.description)\n"
-  }
+    override var description: String  {
+        return "\(super.description)\n"
+    }
 }
 
 class Definition: NonTerminalClass {
-  override var description: String  {
-    return "\(super.description)\n"
-  }
+    override var description: String  {
+        return "\(super.description)\n"
+    }
 }
 
 
